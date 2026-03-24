deed1d33514d664d1c36d86357349342cb8052a6
YashBhoir20 <byash24comp@student.mes.ac.in>
2026-03-24 19:49:27 +0530

    Update ProjectMember.js

--- Backend/src/models/ProjectMember.js
+++ Backend/src/models/ProjectMember.js
@@ -1,1 +1,11 @@
+const mongoose = require("mongoose");
 
+const projectMemberSchema = new mongoose.Schema(
+  {
+    user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    project_id: { type: mongoose.Schema.Types.ObjectId, ref: "Project", required: true }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("ProjectMember", projectMemberSchema);
