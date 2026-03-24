3e5603e3b2fb14e723f26d306457d3f1e3f84cc7
YashBhoir20 <byash24comp@student.mes.ac.in>
2026-03-24 19:52:19 +0530

    Update projectValidators.js

--- Backend/src/validators/projectValidators.js
+++ Backend/src/validators/projectValidators.js
@@ -1,1 +1,15 @@
+const { body } = require("express-validator");
 
+const projectValidator = [
+  body("title").notEmpty().withMessage("Project title is required"),
+  body("description").notEmpty().withMessage("Description is required"),
+  body("domain")
+    .isIn(["Astronomy", "Rockets", "Satellites", "Robotics", "Space Biology"])
+    .withMessage("Invalid domain"),
+  body("status")
+    .optional()
+    .isIn(["Open", "In Progress", "Completed"])
+    .withMessage("Invalid status")
+];
+
+module.exports = { projectValidator };
