4051cb7326afba7f74c0672a790deaeeba5fdfe2
YashBhoir20 <byash24comp@student.mes.ac.in>
2026-03-24 19:48:40 +0530

    Update Project.js

--- Backend/src/models/Project.js
+++ Backend/src/models/Project.js
@@ -1,1 +1,33 @@
+const mongoose = require("mongoose");
 
+const projectSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, trim: true },
+    description: { type: String, required: true },
+    created_by: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+    domain: {
+      type: String,
+      enum: ["Astronomy", "Rockets", "Satellites", "Robotics", "Space Biology"],
+      required: true
+    },
+    status: {
+      type: String,
+      enum: ["Open", "In Progress", "Completed"],
+      default: "Open"
+    },
+    members: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+      }
+    ],
+    files: [{ type: String }]
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Project", projectSchema);
