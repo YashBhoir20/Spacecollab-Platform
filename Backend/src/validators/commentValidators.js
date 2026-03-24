21219d9c61a1e7659c1c27d46582868dd304b83e
YashBhoir20 <byash24comp@student.mes.ac.in>
2026-03-24 19:52:35 +0530

    Update commentValidators.js

--- Backend/src/validators/commentValidators.js
+++ Backend/src/validators/commentValidators.js
@@ -1,1 +1,7 @@
+const { body } = require("express-validator");
 
+const commentValidator = [
+  body("content").notEmpty().withMessage("Comment content is required")
+];
+
+module.exports = { commentValidator };
