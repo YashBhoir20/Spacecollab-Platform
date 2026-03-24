38400423c3e4465a5c9893fba7d38864fe782908
YashBhoir20 <byash24comp@student.mes.ac.in>
2026-03-24 19:54:14 +0530

    Update commentController.js

--- Backend/src/controllers/commentController.js
+++ Backend/src/controllers/commentController.js
@@ -1,1 +1,22 @@
+const Comment = require("../models/Comment");
 
+const getCommentsByProject = async (req, res) => {
+  const comments = await Comment.find({ project_id: req.params.projectId })
+    .populate("user_id", "name role")
+    .sort({ createdAt: -1 });
+
+  res.json(comments);
+};
+
+const addComment = async (req, res) => {
+  const comment = await Comment.create({
+    user_id: req.user._id,
+    project_id: req.params.projectId,
+    content: req.body.content
+  });
+
+  const populated = await comment.populate("user_id", "name role");
+  res.status(201).json(populated);
+};
+
+module.exports = { getCommentsByProject, addComment };
