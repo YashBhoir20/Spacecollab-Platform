29dd4c15179f140c6a3a4a3229b7f89dcf9d9fe8
YashBhoir20 <byash24comp@student.mes.ac.in>
2026-03-24 19:55:08 +0530

    Update authRoutes.js

--- Backend/src/routes/authRoutes.js
+++ Backend/src/routes/authRoutes.js
@@ -1,1 +1,11 @@
+const express = require("express");
+const { registerUser, loginUser } = require("../controllers/authController");
+const { registerValidator, loginValidator } = require("../validators/authValidators");
+const validate = require("../middleware/validateMiddleware");
 
+const router = express.Router();
+
+router.post("/register", registerValidator, validate, registerUser);
+router.post("/login", loginValidator, validate, loginUser);
+
+module.exports = router;
