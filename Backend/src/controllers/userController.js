5396d4b5325c2d743b7b6d8feafa34c99ee49fb8
YashBhoir20 <byash24comp@student.mes.ac.in>
2026-03-24 19:53:28 +0530

    Update userController.js

--- Backend/src/controllers/userController.js
+++ Backend/src/controllers/userController.js
@@ -1,1 +1,26 @@
+const User = require("../models/User");
+const Project = require("../models/Project");
 
+const getProfile = async (req, res) => {
+  const user = await User.findById(req.user._id).select("-password");
+  const projects = await Project.find({ members: req.user._id });
+  res.json({ user, projects });
+};
+
+const updateProfile = async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (!user) {
+    res.status(404);
+    throw new Error("User not found");
+  }
+
+  user.name = req.body.name || user.name;
+  user.bio = req.body.bio || user.bio;
+  user.avatar = req.body.avatar || user.avatar;
+
+  const updated = await user.save();
+  res.json(updated);
+};
+
+module.exports = { getProfile, updateProfile };
