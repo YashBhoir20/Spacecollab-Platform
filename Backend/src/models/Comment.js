dedfe8cafe08402c3784c161bb781352cb168197
YashBhoir20 <byash24comp@student.mes.ac.in>
2026-03-24 19:49:01 +0530

    Update Comment.js

--- Backend/src/models/Comment.js
+++ Backend/src/models/Comment.js
@@ -1,1 +1,20 @@
+const mongoose = require("mongoose");
 
+const commentSchema = new mongoose.Schema(
+  {
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+    project_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Project",
+      required: true
+    },
+    content: { type: String, required: true }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Comment", commentSchema);
