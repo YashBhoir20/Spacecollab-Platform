cf3cda54e2a97828afe532982dcd0f5598c0a3a2
YashBhoir20 <byash24comp@student.mes.ac.in>
2026-03-24 19:55:57 +0530

    Update projectRoutes.js

--- Backend/src/routes/projectRoutes.js
+++ Backend/src/routes/projectRoutes.js
@@ -1,1 +1,23 @@
+const express = require("express");
+const {
+  getProjects,
+  getProjectById,
+  createProject,
+  updateProject,
+  deleteProject,
+  joinProject
+} = require("../controllers/projectController");
+const { protect } = require("../middleware/authMiddleware");
+const validate = require("../middleware/validateMiddleware");
+const { projectValidator } = require("../validators/projectValidators");
 
+const router = express.Router();
+
+router.get("/", getProjects);
+router.get("/:id", getProjectById);
+router.post("/", protect, projectValidator, validate, createProject);
+router.put("/:id", protect, projectValidator, validate, updateProject);
+router.delete("/:id", protect, deleteProject);
+router.post("/:id/join", protect, joinProject);
+
+module.exports = router;
