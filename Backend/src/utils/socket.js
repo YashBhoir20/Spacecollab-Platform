cc2e83da30bd3bd2af1634b3b9c6b1454428ee13
YashBhoir20 <byash24comp@student.mes.ac.in>
2026-03-24 19:57:05 +0530

    Update socket.js

--- Backend/src/utils/socket.js
+++ Backend/src/utils/socket.js
@@ -1,1 +1,23 @@
+const initSocket = (io) => {
+  io.on("connection", (socket) => {
+    console.log("User connected:", socket.id);
 
+    socket.on("joinProjectRoom", (projectId) => {
+      socket.join(projectId);
+    });
+
+    socket.on("sendProjectMessage", ({ projectId, message, user }) => {
+      io.to(projectId).emit("receiveProjectMessage", {
+        user,
+        message,
+        createdAt: new Date()
+      });
+    });
+
+    socket.on("disconnect", () => {
+      console.log("User disconnected:", socket.id);
+    });
+  });
+};
+
+module.exports = initSocket;
