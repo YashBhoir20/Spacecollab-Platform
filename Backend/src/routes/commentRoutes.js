4404350c4bd48c2dae3b555d00dbc61d5058469d
YashBhoir20 <byash24comp@student.mes.ac.in>
2026-03-24 19:56:20 +0530

    Update commentRoutes.js

--- Backend/src/routes/commentRoutes.js
+++ Backend/src/routes/commentRoutes.js
@@ -1,1 +1,12 @@
+const express = require("express");
+const { getCommentsByProject, addComment } = require("../controllers/commentController");
+const { protect } = require("../middleware/authMiddleware");
+const { commentValidator } = require("../validators/commentValidators");
+const validate = require("../middleware/validateMiddleware");
 
+const router = express.Router();
+
+router.get("/:projectId", getCommentsByProject);
+router.post("/:projectId", protect, commentValidator, validate, addComment);
+
+module.exports = router;
