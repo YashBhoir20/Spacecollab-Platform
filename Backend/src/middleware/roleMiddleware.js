7e87e1bebfc44d998150d766161377b4b9689c99
YashBhoir20 <byash24comp@student.mes.ac.in>
2026-03-24 19:50:44 +0530

    Update roleMiddleware.js

--- Backend/src/middleware/roleMiddleware.js
+++ Backend/src/middleware/roleMiddleware.js
@@ -1,1 +1,11 @@
+const allowRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!req.user || !roles.includes(req.user.role)) {
+      res.status(403);
+      throw new Error("Access denied");
+    }
+    next();
+  };
+};
 
+module.exports = allowRoles;
